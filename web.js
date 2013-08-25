50c0598b9c76869fca9344863a37c499d2f3359d
Mehnaz Singh <msjaggi@yahoo.com>
2013-08-25 19:52:53 +0000

    upate web.js second time

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var fs = require('fs');
+var b = new Buffer(30);
+var data = fs.readFileSync('index.html');
+response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
